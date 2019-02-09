ce86f19aada0fc0599cd7a786dc7f48eebbb09da
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-02-08 23:51:51 -0300

    Restructure data.js to be a object

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,169 +1,169 @@
-const acronyms = [
-    {
+const acronyms = {
+    CC: [{
         acronym: 'CC',
         meaning: 'Ciência da Computação',
         type: 'Curso'
-    },
-    {
+    }],
+    LCC1: [{
         acronym: 'LCC1',
         meaning: 'Laboratório de ciência da computação 1',
         type: 'Sala'
-    },
-    {
+    }],
+    LCC2: [{
         acronym: 'LCC2',
         meaning: 'Laboratório de ciência da computação 2',
         type: 'Sala'
-    },
-    {
+    }],
+    LCC3: [{
         acronym: 'LCC3',
         meaning: 'Laboratório de ciência da computação 3',
         type: 'Sala'
-    },
-    {
+    }],
+    EDA: [{
         acronym: 'EDA',
         meaning: 'Estrutura de Dados e Algoritmos',
         type: 'Disciplina'
-    },
-    {
+    }],
+    LEDA: [{
         acronym: 'LEDA',
         meaning: 'Laboratório de Estrutura de Dados e Algoritmos',
         type: 'Disciplina'
-    },
-    {
+    }],
+    P1: [{
         acronym: 'P1',
         meaning: 'Programação 1',
         type: 'Disciplina'
-    },
-    {
+    }],
+    LP1: [{
         acronym: 'LP1',
         meaning: 'Laboratório de Programação 1',
         type: 'Disciplina'
-    },
-    {
+    }],
+    P2: [{
         acronym: 'P2',
         meaning: 'Programação 2',
         type: 'Disciplina'
-    },
-    {
+    }],
+    LP2: [{
         acronym: 'LP2',
         meaning: 'Laboratório de Programação 2',
         type: 'Disciplina'
-    },
-    {
+    }],
+    IC: [{
         acronym: 'IC',
         meaning: 'Introdução à Computação',
         type: 'Disciplina'
-    },
-    {
+    }],
+    BD: [{
         acronym: 'BD',
         meaning: 'Banco de Dados I',
         type: 'Disciplina'
-    },
-    {
+    }],
+    OAC: [{
         acronym: 'OAC',
         meaning: 'Organização e Arquitetura de Computadores',
         type: 'Disciplina'
-    },
-    {
+    }],
+    LOAC: [{
         acronym: 'LOAC',
         meaning: 'Laboratório de Organização e Arquitetura de Computadores',
         type: 'Disciplina'
-    },
-    {
+    }],
+    FMCC1: [{
         acronym: 'FMCC1',
         meaning: 'Fundamentos de Matemática para Ciência da Computação I',
         type: 'Disciplina'
-    },
-    {
+    }],
+    FMCC2: [{
         acronym: 'FMCC2',
         meaning: 'Fundamentos de Matemática para Ciência da Computação II',
         type: 'Disciplina'
-    },
-    {
+    }],
+    PLP: [{
         acronym: 'PLP',
         meaning: 'Paradigmas de Linguagem de Programação',
         type: 'Disciplina'
-    },
-    {
+    }],
+    ES: [{
         acronym: 'ES',
         meaning: 'Engenharia de Software',
         type: 'Disciplina'
-    },
-    {
+    }],
+    SO: [{
         acronym: 'SO',
         meaning: 'Sistemas Operacionais',
         type: 'Disciplina'
-    },
-    {
+    }],
+    TC: [{
         acronym: 'TC',
         meaning: 'Teoria da Computação',
         type: 'Disciplina'
-    },
-    {
+    }],
+    IA: [{
         acronym: 'IA',
         meaning: 'Inteligência Artificial',
         type: 'Disciplina'
-    },
-    {
+    }],
+    PC: [{
         acronym: 'PC',
         meaning: 'Programação Concorrente',
         type: 'Disciplina'
-    },
-    {
+    }],
+    ATAL: [{
         acronym: 'ATAL',
         meaning: 'Análise e Técnicas de Algoritmos',
         type: 'Disciplina'
-    },
-    {
+    }],
+    TCC: [{
         acronym: 'TCC',
         meaning: 'Trabalho de Conclusão de Curso',
         type: 'Disciplina'
-    },
-    {
+    }],
+    CDD: [{
         acronym: 'CDD',
         meaning: 'Ciência de Dados Descritiva',
         type: 'Disciplina'
-    },
-    {
+    }],
+    CDP: [{
         acronym: 'CDP',
         meaning: 'Ciência de Dados Preditiva',
         type: 'Disciplina'
-    },
-    {
+    }],
+    ADSD: [{
         acronym: 'ADSD',
         meaning: 'Avaliação de Desempenho de Sistemas Discretos',
         type: 'Disciplina'
-    },
-    {
+    }],
+    BD2: [{
         acronym: 'BD2',
         meaning: 'Banco de Dados II',
         type: 'Disciplina'
-    },
-    {
+    }],
+    IRC: [{
         acronym: 'IRC',
         meaning: 'Interconexão e Redes de Computadores',
         type: 'Disciplina'
-    },
-    {
+    }],
+    AA: [{
         acronym: 'AA',
         meaning: 'Algoritmos Avançados',
         type: 'Disciplina'
-    },
-    {
+    }],
+    ETI: [{
         acronym: 'ETI',
         meaning: 'Economia de Tecnologia da Informação',
         type: 'Disciplina'
-    },
-    {
+    }],
+    DACA: [{
         acronym: 'DACA',
         meaning: 'Desenvolvimento de Aplicações Corporativas Avançadas',
         type: 'Disciplina'
-    },
-    {
+    }],
+    SAD: [{
         acronym: 'SAD',
         meaning: 'Sistemas de Apoio à Decisão',
         type: 'Disciplina'
-    }
-];
+    }]
+};
 
 export default acronyms
