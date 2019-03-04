98a40cd3d65195058983329eb97809393b1b9855
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2019-03-04 17:33:35 -0300

    atualizando script para incluir legacy, e atualizando merge de legacy

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,7 @@
 import cursos from './cursos.json';
 import disciplinas from './disciplinas.json';
 import girias from './girias.json';
+import legacy from './legacy.json';
 import locais from './locais.json';
 import outros from './outros.json';
 
@@ -19,6 +20,7 @@ const acronyms = { };
 merge(acronyms, cursos);
 merge(acronyms, disciplinas);
 merge(acronyms, girias);
+merge(acronyms, legacy)
 merge(acronyms, locais);
 merge(acronyms, outros);
 
