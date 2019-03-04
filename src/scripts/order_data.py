87513dcb4c34f4292afcc8a19f5cc030506f814a
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2019-03-04 00:14:21 -0300

    salvando alterações

--- src/scripts/order_data.py
+++ src/scripts/order_data.py
@@ -2,41 +2,26 @@
 # (C) Júlio Barreto
 # Script para ordenar os dados do data.js por nome
 # Criado em 16/02/2019
+# Atualizado em 03/03/2019
 
-import json, sys, demjson
+import json, sys
 
-def remove_indentation(str_list):
-	"""
-	Remove a indentação do objeto JavaScript de modo que ele
-	possa ser decodificado normalmente
-	"""
-	return ''.join([i.strip() for i in str_list])
-	
+def update_file(name):
+    with open(name, 'r') as datajs:
+        """
+        Abre o arquivo atual de dados e extrai o json de acronimos
+        """
+        datafile = json.load(datajs)
 
-with open('../lib/data.js', 'r') as datajs:
-	"""
-	Abre o arquivo atual de dados e extrai o json de acronimos
-	"""
-	line_string = ''.join(datajs.readlines())
+    with open(name, 'w') as newdatajs:
+        """
+        Exporta o arquivo de dados de modo que as siglas estejam ordenadas
+        """
+        json_text = json.dumps(datafile, sort_keys=True, indent=4, ensure_ascii=False).encode('utf8')
+        newdatajs.write(json_text)
 
-	data_start = line_string.index('{')
-	data_end = line_string.index('};') + 1
-	before = line_string[:data_start]
-	after = line_string[data_end:]
+path_to_data = '../lib/%s.json'
+data_files = ['cursos', 'disciplinas', 'girias', 'locais', 'outros']
 
-	data_string = line_string[data_start : data_end].split('\n')
-	formatted_data = remove_indentation(data_string)
-	
-	data = demjson.decode(remove_indentation(data_string))
-
-with open('../lib/data.js', 'w') as newdatajs:
-	"""
-	Exporta o arquivo de dados de modo que as siglas estejam ordenadas
-	"""
-	ordered_data = json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False).encode('utf8')
-
-	newdatajs.write(before)
-	newdatajs.write(ordered_data)
-	newdatajs.write(after)
-	
-	
+for df in data_files:
+    update_file(path_to_data % df)
