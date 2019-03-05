0d8a06ed19cda560dc672362de5da1156b238f29
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-03-05 01:22:04 -0300

    Conserta cores dos componentes do glossario

--- src/components/IconWithText/IconWithText.js
+++ src/components/IconWithText/IconWithText.js
@@ -4,7 +4,7 @@ import "./IconWithText.css"
 import Icon from "../Icon/Icon"
 
 const IconWithText = (props) => (
-    <div className={"icon-with-text"}>
+    <div className={"icon-with-text light-accent"}>
         <Icon icon={props.icon}/>
         <span>{props.text}</span>
     </div>
