40c516b566c4e9163dde572b9e2b2af7025e02e9
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-03-06 14:31:31 -0300

    Replace BrowserRouter by HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import 'antd/dist/antd.css';
 import './index.css';
@@ -11,7 +11,7 @@ import './styles/odu/colors.css';
 import './styles/utils.css';
 
 ReactDOM.render((
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 ), document.getElementById('app'));
