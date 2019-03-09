3e97c26e55aa154d261d74f1236a0e4db2914c5f
José Renan <joserenansl99@gmail.com>
2019-03-09 19:48:06 -0300

    Corrige issues de revisão

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -3,22 +3,22 @@ import React from 'react';
 import './ResultCard.css';
 
 const Examples = ({ key, examples }) => examples.map((ex, index) => (
-    <li key={`${key}${index}`}>{ ex }</li>
+    <li className={"result-card__list-phrase"} key={`${key}${index}`}>{ ex }</li>
 ));
 
 const ResultCard = ({ result }) => (
     <div className={"result-card__container"}>
-        <h1>{ result.entry }</h1>
+        <h1 className={"result-card__title-1"}>{ result.entry }</h1>
         <span className={"result-card__subtitle"}>{ result.type }</span>
 
         <div className="result-card__content">
-            <h2>Descrição</h2>
+            <h2 className={"result-card__title-2"}>Descrição</h2>
             <p>{ result.meaning }</p>
         </div>
 
         <div className="result-card__content">
-            <h2>Aplicações em frase</h2>
-            <ul>
+            <h2 className={"result-card__title-2"}>Aplicações em frase</h2>
+            <ul className={"result-card__list-phrase"}>
                 <Examples key={ result.entry } examples={ result.examples } />
             </ul>
         </div>
