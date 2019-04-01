dedbda33648bcdf8b0bb1322425654533bd81148
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2019-04-01 04:01:13 -0300

    fixing orderData error

--- src/scripts/orderData.js
+++ src/scripts/orderData.js
@@ -25,8 +25,8 @@ function updateFile(name) {
     fs.writeFileSync(path, dataString);
 }
 
-console.log('\0o33[1;93m Reordenando arquivos de dados... \0o33[0m');
+console.log('\033[1;93m Reordenando arquivos de dados... \033[0m');
 
 dataFiles.forEach(df => updateFile(df));
 
-console.log('\0o33[1;93m Pronto! :) \0o33[0m');
+console.log('\033[1;93m Pronto! :) \033[0m');
