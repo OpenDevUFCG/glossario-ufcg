413cfe258bb44b3ef5fc9342a53def770b1d34bc
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-08-01 10:04:11 -0300

    Remove unused imports

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
