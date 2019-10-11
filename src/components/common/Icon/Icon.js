89fa53a392beb886d35b3ade10637e314074fa66
Jonathan <jonathan.henrique.smtp@gmail.com>
2019-10-11 15:35:15 -0300

    renomeando propTypes 'color' -> iconColor'

--- src/components/common/Icon/Icon.js
+++ src/components/common/Icon/Icon.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Icon = ({ icon, color, size }) => (
-  <i className="material-icons" style={{ color: color, fontSize: size }}>
+const Icon = ({ icon, iconColor, size }) => (
+  <i className="material-icons" style={{ color: iconColor, fontSize: size }}>
     {icon}
   </i>
 );
 
 Icon.propTypes = {
   icon: PropTypes.string,
-  color: PropTypes.string,
+  iconColor: PropTypes.string,
   size: PropTypes.string,
 };
 
